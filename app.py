256f0f05758fe792dc176cb14ddb18cace8eb68e
harish <harishezhil1431@gmail.com>
2025-01-21 22:59:15 +0530

    Updated app for deployment

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import pyodbc
 
 app = Flask(__name__)
+
+# Database connection
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=<your_server>.database.windows.net;'
+    'DATABASE=StudentDB;'
+    'UID=<your_username>;'
+    'PWD=<your_password>'
+)
+cursor = conn.cursor()
 
 @app.route('/')
 def home():
     return "Hello, Azure Flask App!"
+
+# CRUD operations here...
 
 if __name__ == '__main__':
     app.run(debug=True)
